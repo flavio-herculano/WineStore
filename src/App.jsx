590c727aac9bf3ec40e92075f895a2955a579546
Flávio Herculano <flavio@setorinformatica.com>
2022-11-09 07:46:06 -0300

    adicionado pagina Cadastro e import React

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-
+import React from "react";
 import "./App.css";
-import Login from "./components/Login";
+import Login from "components/Login";
+import Register from "components/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/Entrar" element={<Login />} />
+        <Route exact path="/Cadastro" element={<Register />} />
       </Routes>
     </Router>
   );
