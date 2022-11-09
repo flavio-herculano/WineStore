d286c8cf0dd9d796dd8c8d01fea5999ad7b55b4e
Flávio Herculano <flavio@setorinformatica.com>
2022-11-09 10:21:01 -0300

    Adicionado categorias e product nas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ import "./App.css";
 import Login from "components/Login";
 import Register from "components/Register";
 import Dashboard from "components/Dashboard";
+import VinhoBranco from "components/Home/VinhoBranco";
+import VinhoSeco from "components/Home/VinhoSeco";
+import VinhoTinto from "components/Home/VinhoTinto";
+import Product from "components/Home/Product";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/Produto/:id" element={<Product />} />
+
+        <Route exact path="/Vinho_Branco" element={<VinhoBranco />} />
+        <Route exact path="/Vinho_Seco" element={<VinhoSeco />} />
+        <Route exact path="/Vinho_Tinto" element={<VinhoTinto />} />
         <Route exact path="/Entrar" element={<Login />} />
         <Route exact path="/Cadastro" element={<Register />} />
         <Route exact path="/Dashboard" element={<Dashboard />} />
